4b7d6eac35267aed5c3f6fc06e4ff033c12b8f39
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-11-09 11:37:44 +0530

    BMICalculator

---
+++ JS/first.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Compose your tweet:");
+var tweetcount = tweet.length
+alert("You have written" + tweetcount + "character, you have" + (240 - tweetcount) + "characters remaining");
